e135bf7d3f10811e14ad417e43460fc5e7d8b72e
Mahmoud_Madih <moodbeder150@gmail.com>
2024-03-29 20:54:35 +0200

    no message

---
+++ lib/core/Calculator.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class calculator extends StatefulWidget {
+  const calculator({super.key});
+
+  @override
+  State<calculator> createState() => _calculatorState();
+}
+
+class _calculatorState extends State<calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:
+        const Text('calculatetor', style: TextStyle(fontWeight:  FontWeight.bold, fontSize: 20,),),
+
+
+      ),
+    );
+  }
+}
